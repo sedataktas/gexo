5af83dea9dfd5e35fef66c0bca28c098b1be4b9a
Sedat Aktas <sedata38@gmail.com>
2020-12-30 01:53:22 +0300

    added getFile added linux commands

--- main.go
+++ main.go
@@ -3,22 +3,50 @@ package main
 import (
 	"fmt"
 	"golang.org/x/term"
+	"io/ioutil"
 	"os"
+	"os/exec"
 )
 
 func main() {
-	fd := int(os.Stdin.Fd())
+	f := getFile()
 
+	fd := int(os.Stdin.Fd())
 	state, err := term.MakeRaw(fd)
 	if err != nil {
 		fmt.Errorf("terminal raw error:%v", err)
 	}
 	defer term.Restore(fd, state)
 
-	newTerminal := term.NewTerminal(os.Stdin, ">")
-	line, err := newTerminal.ReadLine()
-	if err != nil {
-		fmt.Println(err)
+	//term.NewTerminal(os.Stdin,">>>>>")
+
+	// disable input buffering
+	exec.Command("stty", "-F", "/dev/tty", "cbreak", "min", "1").Run()
+	// do not display entered characters on the screen
+	exec.Command("stty", "-F", "/dev/tty", "-echo").Run()
+
+	os.Stdin.Write(f)
+	var b = make([]byte, 1)
+	for {
+		os.Stdin.Read(b)
+		fmt.Print(string(b))
+		if string(b) == "q" {
+			exec.Command("stty", "-raw").Run()
+		}
 	}
-	fmt.Println(line)
+}
+
+func getFile() []byte {
+	fileName := os.Args[1]
+
+	f, err := os.Open(fileName)
+	if err != nil {
+		panic(err)
+	}
+
+	fByteArr, err := ioutil.ReadAll(f)
+	if err != nil {
+		panic(err)
+	}
+	return fByteArr
 }
